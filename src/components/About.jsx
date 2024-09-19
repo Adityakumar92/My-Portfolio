3a4dc906a1f7a8295add4500caeb2574dcc67828
Adityakumar92 <aditya.kumar.che21@itbhu.ac.in>
2024-09-20 02:28:42 +0530

    color adjustment

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ function About() {
     >
       <div>
         <h1 className="text-3xl font-bold mb-5">About</h1>
-        <p className="bg-green-100">
+        <p className="bg-zinc-100">
           Hi, I'm Aditya Kumar, a dedicated web developer and programmer currently pursuing my studies at the Indian Institute of Technology (BHU), Varanasi. With a passion for crafting dynamic web experiences and solving complex coding challenges, I thrive at the intersection of creativity and technology. I am continuously honing my skills to create seamless, responsive, and innovative digital solutions.
         </p>
         <br />
@@ -16,7 +16,7 @@ function About() {
           Education & Training
         </h1>
         <table className="table-auto w-[100%]">
-          <thead className="bg-green-300">
+          <thead className="bg-zinc-200">
             <tr>
               <th>Degree/Certification</th>
               <th>Institution</th>
@@ -24,7 +24,7 @@ function About() {
               <th>Year</th>
             </tr>
           </thead>
-          <tbody className="bg-green-100">
+          <tbody className="bg-zinc-100">
             <tr>
               <td className="text-center">CBSE (XII)</td>
               <td className="text-center">Kendriya Vidyalaya BHU Campus Varanasi</td>
@@ -49,13 +49,13 @@ function About() {
         <h1 className="text-green-600 font-semibold text-xl">
           Skills & Expertise
         </h1>
-        <div className="bg-green-200 mt-2">
+        <div className="bg-zinc-200 mt-2">
           <span className="font-semibold">Proficient in : </span>C++, JavaScript
         </div>
-        <div className="bg-green-200 mt-2">
+        <div className="bg-zinc-200 mt-2">
           <span className="font-semibold">Experienced with : </span>React JS, MongoDB, Express, Node Js
         </div>
-        <div className="bg-green-200 mt-2">
+        <div className="bg-zinc-200 mt-2">
           <span className="font-semibold">Strong grasp of : </span>JavaScript, Data Structure and Algorithms
         </div>
         <br />
@@ -65,16 +65,16 @@ function About() {
           Professional Experience
         </h1>
         <div className="mt-2">
-          <div className=" bg-green-300 flex items-center justify-between">
+          <div className=" bg-zinc-200 flex items-center justify-between">
             <span className="font-semibold">Frontend Developer Intern | BLUESTOCK</span>
             <span className="font-semibold">1/july/2024 - 30/july/2024</span>
           </div>
           <div>
             <ul>
-              <li className="bg-green-100 mt-1">Spearheaded the design and development of a robust admin web portal, leveraging HTML, CSS, and JavaScript to deliver a seamless user experience.</li>
-              <li className="bg-green-100 mt-1">Implemented cutting-edge authentication and authorization mechanisms, ensuring secure user verification and role based redirection to tailored dashboards.</li>
-              <li className="bg-green-100 mt-1">Crafted a dynamic, data-driven admin dashboard, showcasing key performance metrics, including active user count, admin placement, and real-time analytics.</li>
-              <li className="bg-green-100 mt-1">Developed a fully responsive, mobile-first website, utilizing CSS media queries, flexible grids, and JavaScript event listeners to ensure a consistent, intuitive interface across devices.</li>
+              <li className="bg-zinc-100 mt-1">Spearheaded the design and development of a robust admin web portal, leveraging HTML, CSS, and JavaScript to deliver a seamless user experience.</li>
+              <li className="bg-zinc-100 mt-1">Implemented cutting-edge authentication and authorization mechanisms, ensuring secure user verification and role based redirection to tailored dashboards.</li>
+              <li className="bg-zinc-100 mt-1">Crafted a dynamic, data-driven admin dashboard, showcasing key performance metrics, including active user count, admin placement, and real-time analytics.</li>
+              <li className="bg-zinc-100 mt-1">Developed a fully responsive, mobile-first website, utilizing CSS media queries, flexible grids, and JavaScript event listeners to ensure a consistent, intuitive interface across devices.</li>
             </ul>
           </div>
         </div>
@@ -84,14 +84,14 @@ function About() {
           Extra-Curricular Activities
         </h1>
         <div className="mt-2">
-          <div className=" bg-green-300 flex items-center justify-between">
+          <div className=" bg-zinc-200 flex items-center justify-between">
             <span className="font-semibold">Marketing Executive | JAGRITI ' 23 | Annual Social Service Fest Of IIT-BHU</span>
             <span className="font-semibold">july/2023 - April/2023</span>
           </div>
           <div>
             <ul>
-              <li className="bg-green-100 mt-1">Lead acquring sponsorship from multiple local and national brands, thus enchancing the quality of fest.</li>
-              <li className="bg-green-100 mt-1">Devised optimal 'PublicityPoint' for for maximizing the sponsors reach in offline and online events in JAGRITI'23.</li>
+              <li className="bg-zinc-100 mt-1">Lead acquring sponsorship from multiple local and national brands, thus enchancing the quality of fest.</li>
+              <li className="bg-zinc-100 mt-1">Devised optimal 'PublicityPoint' for for maximizing the sponsors reach in offline and online events in JAGRITI'23.</li>
             </ul>
           </div>
         </div>
@@ -100,7 +100,7 @@ function About() {
         <h1 className="text-green-600 font-semibold text-xl">
           Mission Statement
         </h1>
-        <p className="bg-green-100 mt-2">
+        <p className="bg-zinc-100 mt-2">
           My mission is to leverage my skills and creativity to deliver cutting-edge web development and programming solutions that not only meet but exceed client expectations. I am dedicated to creating impactful digital experiences that enhance the user journey and contribute positively to the evolving digital landscape. With a commitment to continuous learning and innovation, I am always seeking new challenges and opportunities to push the boundaries of web technology.
         </p>
       </div>
