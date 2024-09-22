6b414f8594759f740dca830b20fd182ecb715b9d
Adityakumar92 <aditya.kumar.che21@itbhu.ac.in>
2024-09-22 23:24:33 +0530

    add new project

--- src/components/PortFolio.jsx
+++ src/components/PortFolio.jsx
@@ -4,6 +4,7 @@ import book from "../../public/Book.png"
 import chatter from "../../public/chat-logo.avif"
 import notepade from "../../public/notepade.jpg"
 import genralWebsite from "../../public/website.png"
+import sortify from "../../public/sortify.png"
 
 function PortFolio() {
   const cardItem = [
@@ -11,32 +12,44 @@ function PortFolio() {
       id: 1,
       logo: chatter,
       name: "Chatter-Box",
-      link: "https://github.com/Adityakumar92/Chatter-Box"
+      link: "https://github.com/Adityakumar92/Chatter-Box",
+      tag: "Real-time messaging, anytime, anywhere."
     },
     {
       id: 2,
       logo: book,
       name: "BookStore Application",
-      link: "https://github.com/Adityakumar92/BookStore"
+      link: "https://github.com/Adityakumar92/BookStore",
+      tag: "Your next great read is just a click away."
     },
     {
       id: 3,
       logo: news,
       name: "News App",
-      link: "https://github.com/Adityakumar92/News-App"
+      link: "https://github.com/Adityakumar92/News-App",
+      tag: "Your daily dose of headlines and stories."
     },
     {
       id: 4,
       logo: notepade,
       name: "Notepade",
-      link: "https://github.com/Adityakumar92/iNoteBook"
+      link: "https://github.com/Adityakumar92/iNoteBook",
+      tag: "Your personal space for notes and ideas."
     },
     {
       id: 5,
       logo: genralWebsite,
       name: "Genral Website",
-      link: "https://github.com/Adityakumar92/General_website"
+      link: "https://github.com/Adityakumar92/General_website",
+      tag: "Flexible layouts for any project."
     },
+    {
+      id: 6,
+      logo: sortify,
+      name: "Sort-ify",
+      link: "https://github.com/Adityakumar92/Sort-ify",
+      tag: "Visualize the magic of sorting."
+    }
   ];
   return (
     <div
@@ -47,7 +60,7 @@ function PortFolio() {
         <h1 className="text-3xl font-bold mb-5">PortFolio</h1>
         <span className=" underline font-semibold">Featured Projects</span>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-3 my-5">
-          {cardItem.map(({ id, logo, name, link }) => (
+          {cardItem.map(({ id, logo, name, link, tag }) => (
             <div
               className="md:w-[300px] md:h-[300px] border-[2px] rounded-lg shadow-lg p-1 cursor-pointer hover:scale-110 hover:bg-zinc-100 duration-300"
               key={id}
@@ -60,13 +73,10 @@ function PortFolio() {
               <div>
                 <div className="px-2 font-bold text-xl mb-2">{name}</div>
                 <p className="px-2 text-gray-700">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  {tag}
                 </p>
               </div>
               <div className=" px-6 py-4 space-x-3 justify-around">
-                {/* <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 py-2 rounded">
-                  Video
-                </button> */}
                 <a href={link} target="_blank">
                   <button className="bg-green-500 hover:bg-green-700 text-white font-bold px-4 py-2 rounded">
                     Source code
